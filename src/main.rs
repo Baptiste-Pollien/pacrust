89ef34ed8fab39dfd49ac42cd325a4c54d1278e9
Baptiste-Pollien <baptiste.pollien@live.com>
2021-05-05 13:41:13 +0200

    Moving player

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,10 @@ fn main() {
 
     // load resources
     let file_name = String::from("maze01.txt");
-    let maze = Maze::load_maze(&file_name).unwrap_or_else(|err| {
+    let mut maze = Maze::load_maze(&file_name).unwrap_or_else(|err| {
         eprintln!("Problem loading maze: {}", err);
         process::exit(1);
     });
-
-    let mut cnt = 0;
 
     // game loop
     loop {
@@ -37,17 +35,12 @@ fn main() {
         }
 
         // process movement
-        match input {
-            Entry::Up => println!("up"),
-            _ => println!("Other")
-        };
+        maze.move_player(input);
 
         // process collisions
 
         // check game over
 
         // repeat
-        cnt += 1;
-        println!("{}", cnt);
     }
 }

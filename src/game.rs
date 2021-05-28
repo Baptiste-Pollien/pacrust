dfb7a58caaa65d332161ed704413dd5b90dd356d
Baptiste-Pollien <baptiste.pollien@live.com>
2021-05-28 11:05:00 +0200

    Implementing a second thread for the ghosts

--- src/game.rs
+++ src/game.rs
@@ -17,15 +17,23 @@ use self::position::Position;
 mod tile;
 pub use self::tile::Tile;
 
+/// Module to manage ghost in a different thread
+mod ghosts;
+pub use self::ghosts::Ghosts;
+
+/// Module to manage Maze in different thread
+mod maze;
+pub use self::maze::Maze;
+
 struct Gum {
     pos:  Position,
     mega: bool
 }
 
 pub struct Game {
-    maze:     Vec<Vec<Tile>>,
+    maze:    maze::Maze,
     pacman:  Position,
-    ghosts:  Vec<Position>,
+    ghosts:  Ghosts,
     gums:    Vec<Gum>,
     width:   usize,
     heigth:  usize,
@@ -35,6 +43,8 @@ pub struct Game {
 }
 
 impl Game {
+    /// Load the maze from the given path and return an initialized 
+    /// Game 
     pub fn load_maze(file: impl AsRef<Path>) -> IoRes<Game> {
         let contents                     = fs::read_to_string(file)?;
         let mut ghosts: Vec<Position>    = Vec::new();
@@ -84,9 +94,9 @@ impl Game {
 
         if let Some(pacman) = pacman {
             Ok(Game {
-                maze,
+                maze: Maze::new(maze),
                 pacman,
-                ghosts,
+                ghosts: Ghosts::new(ghosts),
                 gums,
                 width,
                 heigth,
@@ -102,85 +112,40 @@ impl Game {
 
     }
 
-    fn get_tile(&self, row:usize, col:usize) -> Option<&Tile> {
-        if row < self.heigth && col < self.width {
-            Some(&self.maze[row][col])
-        }
-        else {
-            None
-        }
-    }
-
+    /// Return the number of gums left
     pub fn get_nb_gums(&self) -> usize {
         self.gums.len()
     }
 
+    /// Return the number of lives left
     pub fn get_lives(&self) -> u8 {
         self.lives
     }
 
+    /// Return the number of game available at the start
     pub fn get_max_gums(&self) -> usize {
         self.max_gums
     }
 
+    /// Return the score of the game
+    /// (the number of gum eaten)
     pub fn get_score(&self) -> usize {
         self.max_gums as usize - self.gums.len()
     }
 
-    fn make_move(&self, 
-                 current_pos: &Position,
-                 dir: &console::Entry
-    ) -> Position {
-        let mut pos = Position::new(current_pos.row, current_pos.col);
-
-        match dir {
-            console::Entry::Up => {
-                if pos.row == 0 {
-                    pos.row = self.heigth - 1;
-                }
-                else {
-                    pos.row = pos.row - 1;
-                }
-            },
-            console::Entry::Down => {
-                if pos.row == self.heigth - 1 {
-                    pos.row = 0;
-                }
-                else {
-                    pos.row = pos.row + 1;
-                }
-            },
-            console::Entry::Right => {
-                if pos.col == self.width - 1 {
-                    pos.col = 0;
-                }
-                else {
-                    pos.col = pos.col + 1;
-                }
-            },
-            console::Entry::Left => {
-                if pos.col == 0 {
-                    pos.col = self.width - 1;
-                }
-                else {
-                    pos.col = pos.col - 1;
-                }
-            }
-            _ => (),
-        }
-
-        if let Some(tile) = self.get_tile(pos.row, pos.col) {
-            if *tile == Tile::Wall {
-                pos.row = current_pos.row;
-                pos.col = current_pos.col;
-            }
-        }
-
-        pos
+    /// Get a thread safe clone for the ghosts
+    pub fn get_clone_ghosts(&self) -> ghosts::Ghosts {
+        self.ghosts.get_clone()
     }
 
+    /// Get a thread safe clone for the maze
+    pub fn get_clone_maze(&self) -> Maze {
+        self.maze.get_clone()
+    }
+
+    /// Move the player for the given direction
     pub fn move_player(&mut self, dir: &console::Entry) {
-        let player = self.make_move(&self.pacman, &dir);
+        let player = self.maze.make_move(&self.pacman, &dir);
 
         self.pacman = player;
 
@@ -192,39 +157,19 @@ impl Game {
         }
     }
 
-    pub fn move_ghosts(&mut self) {
-        self.ghosts = self.ghosts
-                          .iter()
-                          .map(|pos| -> Position {
-            let mut new_pos;
-            loop {
-                new_pos = self.make_move(pos, &console::Entry::generate_random());
-
-                if new_pos != *pos {
-                    break
-                }
-            }
-            new_pos
-        }).collect();
-    }
-
+    /// Verify if there is any collision between the player
+    /// and the ghosts. In case of a collision, a live is taken.
     pub fn process_collisions(&mut self) {
-        if let Some(_) = self.ghosts.iter().find(|&ghost| {
-            *ghost == self.pacman
-        }){
+        if self.ghosts.check_collision(&self.pacman) {
             self.lives = self.lives - 1;
         }
     }
 
+    /// Display all the game
     pub fn print_screen(&self) {
         console::ansi::clear_screen();
         // Display maze
-        for line in &self.maze {
-            for c in line {
-                print!("{}", c)
-            }
-            println!("")
-        }
+        self.maze.display_maze();
 
         // Display gums
         self.gums.iter().for_each(|gum| {
@@ -233,9 +178,7 @@ impl Game {
         });
 
         // Display ghosts
-        self.ghosts.iter().for_each(|pos| {
-            console::display_at_pos(&pos, 'G');
-        });
+        self.ghosts.display_ghosts();
 
         // Display player
         console::display_at_pos(&self.pacman, 'P');
